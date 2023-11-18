a8b814485418ad4b83094b6310a8a5313c56752f
keitakiuchi <33561711+keitakiuchi@users.noreply.github.com>
2023-11-18 16:16:34 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const textInput = document.getElementById('textInput');
     
     submitButton.addEventListener('click', function() {
-        const textInput = document.getElementById('textInput').value;
+        const inputValue = textInput.value;
         
         // テキストを送信
         fetch('/save-text', {
@@ -11,15 +11,37 @@ document.addEventListener('DOMContentLoaded', function() {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ text: textInput })
+            body: JSON.stringify({ text: inputValue })
         })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
             
             // テキスト入力のクリア
             textInput.value = '';
         })
-            
         .catch(error => console.error('Error:', error));
     });
 });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const submitButton = document.getElementById('submitButton');
+        
+//     submitButton.addEventListener('click', function() {
+//         const textInput = document.getElementById('textInput').value;
+        
+//         // テキストを送信
+//         fetch('/save-text', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ text: textInput })
+//         })
+//         .then(response => response.json())
+//         .then(data => console.log(data))
+            
+//         .catch(error => console.error('Error:', error));
+//     });
+// });
