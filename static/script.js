2b31a71aa599bdcc6f6d4a529a6134da55572e5b
keitakiuchi <33561711+keitakiuchi@users.noreply.github.com>
2023-11-21 00:17:53 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -49,52 +49,52 @@
 // });
 
 // 遅いけど何とかなる //
-document.addEventListener('DOMContentLoaded', function() {
-    const textInput = document.getElementById('textInput');
-    const messageContainer = document.getElementById('messageContainer'); // メッセージコンテナの要素を取得
-    const clearButton = document.getElementById('clearButton'); // クリアボタンの要素を取得
+// document.addEventListener('DOMContentLoaded', function() {
+//     const textInput = document.getElementById('textInput');
+//     const messageContainer = document.getElementById('messageContainer'); // メッセージコンテナの要素を取得
+//     const clearButton = document.getElementById('clearButton'); // クリアボタンの要素を取得
 
-    textInput.addEventListener('keyup', function() {
-        const inputValue = textInput.value;
+//     textInput.addEventListener('keyup', function() {
+//         const inputValue = textInput.value;
 
-        // エンターキーが押された時に入力が空であれば何もしない
-        if (event.key === 'Enter' && !inputValue) {
-            console.log('空の入力では送信されません');
-            return;
-        }
+//         // エンターキーが押された時に入力が空であれば何もしない
+//         if (event.key === 'Enter' && !inputValue) {
+//             console.log('空の入力では送信されません');
+//             return;
+//         }
 
-        // テキストを送信
-        fetch('/save-text', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ text: inputValue })
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+//         // テキストを送信
+//         fetch('/save-text', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ text: inputValue })
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
 
-            // テキスト入力のクリア
-            textInput.value = '';
+//             // テキスト入力のクリア
+//             textInput.value = '';
             
-            // メッセージをクリアする
-            messageContainer.textContent = '';
-        })
-        .catch(error => console.error('Error:', error));
-    });
+//             // メッセージをクリアする
+//             messageContainer.textContent = '';
+//         })
+//         .catch(error => console.error('Error:', error));
+//     });
     
-    clearButton.addEventListener('click', function() {
-        fetch('/clear-texts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => response.json())
-        .then(data => {
-            messageContainer.textContent = 'テーブルはクリアされました！';
-        })
-        .catch(error => console.error('Error:', error));
-    });
-});
+//     clearButton.addEventListener('click', function() {
+//         fetch('/clear-texts', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             messageContainer.textContent = 'テーブルはクリアされました！';
+//         })
+//         .catch(error => console.error('Error:', error));
+//     });
+// });
